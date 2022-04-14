e50fbd6913ffdffa80d4f62bfe5654e6168e5eae
KnorpelSenf <shtrog@gmail.com>
2022-04-14 20:55:42 +0200

    Draft User API

--- user.ts
+++ user.ts
@@ -1,3 +1,26 @@
-// TODO: get stuff from user etc
+import { AbstractApi, Credentials } from "./api.ts";
 
-// TODO: implement other APIs, too
+export interface Device {
+    type: string;
+    // TODO: add remaining
+}
+
+export interface Goal {
+    steps: number;
+    // TODO: add remaining
+}
+
+export class UserApi extends AbstractApi {
+    constructor(credentials: Credentials) {
+        super(credentials);
+    }
+
+    async getDevices(): Promise<Device[]> {
+        const { devices } = await this.call("user", "getdevice");
+        return devices;
+    }
+
+    async getGoals() {
+        return await this.call("user", "getgoals");
+    }
+}
