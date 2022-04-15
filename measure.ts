41d25e16cf1efe88033dec8b427faa3738e6c023
KnorpelSenf <shtrog@gmail.com>
2022-04-15 10:21:30 +0200

    Implement Measure API partially

---
+++ measure.ts
@@ -0,0 +1,80 @@
+import { AbstractApi, Credentials } from "./api.ts";
+
+export interface Measure {
+    updatetime: string;
+    timezone: string;
+    measuregrps: MeasureGroup[];
+    more: number;
+    offset: number;
+}
+
+export interface MeasureGroup {
+    grpid: number;
+    attrib: number;
+    date: number;
+    created: number;
+    category: number;
+    deviceid: string;
+    measures: MeasurePoint[];
+    timezone: string;
+}
+
+export interface MeasurePoint {
+    value: number;
+    type: number;
+    unit: number;
+}
+
+export class MeasureApi extends AbstractApi {
+    constructor(credentials: Credentials) {
+        super(credentials);
+    }
+
+    async getMeasure(parameters: {
+        type: number | number[];
+        category: 1 | 2;
+        startdate?: number;
+        enddate?: number;
+        offset?: number;
+        lastupdate?: number;
+    }): Promise<Measure> {
+        return await this.call("measure", "getmeas", parameters);
+    }
+
+    async *streamMeasures(parameters: Parameters<this["getMeasure"]>[0]) {
+        do {
+            const {
+                measuregrps,
+                more,
+                offset,
+            } = await this.getMeasure(parameters);
+            yield* measuregrps;
+            parameters.offset = more > 0 ? offset : undefined;
+        } while (parameters.offset !== undefined);
+    }
+
+    getActivity() {
+        // TODO: implement
+        throw new Error("Not implemented yet, please open a PR");
+    }
+
+    *streamActivity() {
+        // TODO: implement
+        throw new Error("Not implemented yet, please open a PR");
+    }
+
+    getIntraDayActivity() {
+        // TODO: implement
+        throw new Error("Not implemented yet, please open a PR");
+    }
+
+    getWorkouts() {
+        // TODO: implement
+        throw new Error("Not implemented yet, please open a PR");
+    }
+
+    *streamWorkouts() {
+        // TODO: implement
+        throw new Error("Not implemented yet, please open a PR");
+    }
+}
